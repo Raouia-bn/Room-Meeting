3654a27387d895c842a7ffc3fbeddd4450c1a6f4
raouia19ben <raouia.ben19@gmail.com>
2024-03-31 04:33:55 +0200

    crud reservation

--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -4,16 +4,30 @@ const jwt = require('jsonwebtoken');
 const Reservation = require('../models/Reservation');
 const Room = require('../models/Room');
 const User = require('../models/User');
-
+function formatDate(date) {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
 const requireAuth = require('../middleware/authMiddleware');
 const { sendReservationConfirmationEmail, sendReservationModificationEmail, sendReservationCancellationEmail } = require('../utils/emailUtils');
 
-
-router.get('/reservations', requireAuth, async (req, res) => {
+router.get('/Getreservations', async (req, res) => {
     try {
-        const userId = req.userId;
-        const reservations = await Reservation.find({ user: userId });
-        res.json(reservations);
+        const token = req.cookies.token;
+      
+        
+          const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+          
+        
+            
+        
+    
+        
+        const userId = decodedToken.userId;
+        const reservations = await Reservation.find({ user: userId }).populate('room');
+        res.render('Reservation/meReservation', { reservations });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Erreur lors de la récupération des réservations de l\'utilisateur' });
@@ -21,9 +35,12 @@ router.get('/reservations', requireAuth, async (req, res) => {
 });
 
 
-router.put('/reservations/:id', requireAuth, async (req, res) => {
+router.post('/updatereservations/:id', async (req, res) => {
     try {
-        const userId = req.userId;
+        // Vérification du jeton JWT
+        const token = req.cookies.token;
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decodedToken.userId;
         const reservationId = req.params.id;
         const updatedReservation = req.body;
         
@@ -36,24 +53,21 @@ router.put('/reservations/:id', requireAuth, async (req, res) => {
             return res.status(404).json({ message: 'Salle de réunion non trouvée' });
         }
        
+        // Vérification des champs de la réservation
         if (updatedReservation.heureDebut >= updatedReservation.heureFin) {
             return res.status(400).json({ message: 'L\'heure de fin doit être ultérieure à l\'heure de début' });
         }
 
-        
         const currentDate = new Date();
         if (new Date(updatedReservation.date) < currentDate) {
             return res.status(400).json({ message: 'La date de réservation doit être supérieure ou égale à la date actuelle' });
-        }else if(new Date(updatedReservation.date) == currentDate)
-        { 
+        } else if(new Date(updatedReservation.date) === currentDate) { 
             const currentHour = currentDate.getHours();
             if (parseInt(updatedReservation.heureDebut) < currentHour) {
                 return res.status(400).json({ message: 'L\'heure de début de la réservation doit être supérieure à l\'heure actuelle' });
-            }}
+            }
+        }
 
-      
-
-        
         const existingReservation = await Reservation.findOne({
             _id: { $ne: reservationId }, 
             room: reservation.room,
@@ -73,29 +87,34 @@ router.put('/reservations/:id', requireAuth, async (req, res) => {
         const user = await User.findById(userId); 
         const userEmail = user.email;
         await sendReservationModificationEmail(userEmail, reservation, room.nom);
-
-        res.json({ message: 'Réservation mise à jour avec succès' });
+        res.redirect('/api/crudReservation/Getreservations');
+      
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Erreur lors de la mise à jour de la réservation' });
     }
 });
 
-
-router.delete('/reservations/:id', requireAuth, async (req, res) => {
+router.delete('/delete/:id', async (req, res) => {
     try {
-        const userId = req.userId;
+        const token = req.cookies.token;
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decodedToken.userId;
+        
         const reservationId = req.params.id;
-       
         const reservation = await Reservation.findOne({ _id: reservationId, user: userId });
+        
         if (!reservation) {
             return res.status(404).json({ message: 'Réservation non trouvée' });
         }
+
         const room = await Room.findById(reservation.room);
         if (!room) {
             return res.status(404).json({ message: 'Salle de réunion non trouvée' });
         }
+
         await Reservation.findByIdAndDelete(reservationId);
+
         const user = await User.findById(userId); 
         const userEmail = user.email;
         await sendReservationCancellationEmail(userEmail, reservation, room.nom);
@@ -106,11 +125,36 @@ router.delete('/reservations/:id', requireAuth, async (req, res) => {
         res.status(500).json({ message: 'Erreur lors de l\'annulation de la réservation' });
     }
 });
-
-router.post('/reservations', requireAuth, async (req, res) => {
+router.get('/updateGetReservation/:id', async (req, res) => {
     try {
-        const { roomId, date, heureDebut, heureFin } = req.body;
-        const userId = req.userId; 
+      const reservation = await Reservation.findById(req.params.id);
+      if (!reservation) {
+        return res.status(404).json({ message: 'Chambre non trouvée' });
+      }
+      res.render('Reservation/updateReservation', { reservation: reservation });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+router.get('/Reservation/add/:id', (req, res) => {
+    const roomId = req.params.id;
+    res.render('Reservation/addReservation', { roomId }); 
+});
+
+router.post('/reservations', async (req, res) => {
+    try {
+        const token = req.cookies.token;
+      
+        
+          const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+          
+        
+            
+        
+    
+        
+        const userId = decodedToken.userId;
+        const { roomId, date, heureDebut, heureFin, description } = req.body;
 
         const room = await Room.findById(roomId);
         if (!room) {
@@ -118,22 +162,23 @@ router.post('/reservations', requireAuth, async (req, res) => {
         }
         if (heureDebut >= heureFin) {
             return res.status(400).json({ message: 'L\'heure de fin doit être ultérieure à l\'heure de début' });
-          }
-          const currentDate = new Date();
-          if (new Date(date) < currentDate) {
-            return res.status(400).json({ success: false, message: 'La date de réservation doit être supérieure ou égale à la date actuelle' });
-          }else if (new Date(date) < currentDate)
-          { if (parseInt(heureDebut) < currentHour) {
-            return res.status(400).json({ success: false, message: 'L\'heure de début de la réservation doit être supérieure à l\'heure actuelle' });
-          }}
-      
-         
-         
+        }
+        
+       
+   
+        currentDate = new Date();
+        if (new Date(date) < currentDate) {
+            return res.status(400).json({ message: 'La date de réservation doit être supérieure ou égale à la date actuelle' });
+        } else if (new Date(date) === currentDate) {
+            const currentHour = currentDatee.getHours();
+            if (parseInt(heureDebut) < currentHour) {
+                return res.status(400).json({ message: 'L\'heure de début de la réservation doit être supérieure à l\'heure actuelle' });
+            }
+        }
 
         const existingReservation = await Reservation.findOne({
             room: roomId,
             date: date,
-            
             $or: [
                 { heureDebut: { $lt: heureFin }, heureFin: { $gt: heureDebut } },
                 { heureDebut: { $eq: heureDebut }, heureFin: { $eq: heureFin } }
@@ -149,19 +194,21 @@ router.post('/reservations', requireAuth, async (req, res) => {
             room: roomId,
             date: date,
             heureDebut: heureDebut, 
-            heureFin: heureFin
+            heureFin: heureFin,
+            description: description
         });
 
         const user = await User.findById(userId); 
         const userEmail = user.email;
         await sendReservationConfirmationEmail(userEmail, reservation, room.nom);
-        
-        res.status(201).json({ message: 'Réservation effectuée avec succès', reservation });
+        res.redirect('/api/crudReservation/Getreservations');
+    
     } catch (error) {
         console.error("Erreur lors de la réservation de la salle de réunion :", error); 
         res.status(500).json({ message: 'Erreur lors de la réservation de la salle de réunion' });
     }
 });
+
 router.get('/rooms/:roomId/reservations', async (req, res) => {
     try {
         const roomId = req.params.roomId;
@@ -172,5 +219,6 @@ router.get('/rooms/:roomId/reservations', async (req, res) => {
         console.error(error);
         res.status(500).send('Erreur lors du rendu de la page des réservations de la salle');
     }
-  });
+});
+
 module.exports = router;
